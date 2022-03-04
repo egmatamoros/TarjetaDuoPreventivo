7e2c4a99d34e3d1a336805aef86e7a7467aae9f9
Erick Garcia <erick.garcia.miston@gmail.com>
2022-03-04 08:19:51 -0600

    proyecto en blanco

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row class="bl">
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
